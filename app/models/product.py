64384d3aa1c0d11497893e1fb5a63db9049e4df5
Alina <s222466@student.uek.krakow.pl>
2022-06-26 23:05:30 +0200

    2022-06-26 AM logic structure

---
+++ app/models/product.py
@@ -0,0 +1,143 @@
+from bs4 import BeautifulSoup
+import requests, json, io
+import pandas as pd
+
+
+from app.models.opinion import Opinion
+from app.models.utils import InvalidIdError
+
+class Product():
+  def __init__(self, id, name="", averageScore=0):
+    self.id = id
+    self.name = name
+    self.opinions = []
+    self.averageScore = averageScore
+
+  # Get Product Name 
+  def extract_name(product_page_soup):
+    if product_page_soup.find("h1", class_="product-top__product-info__name"):
+      return product_page_soup.find("h1", class_="product-top__product-info__name").text
+    return ""
+  
+  # Get Product Score 
+  def extract_score(product_page_soup):
+    if product_page_soup.find("span", class_="product-review__score"):
+      return float(product_page_soup.find("span", class_="product-review__score")['content'])
+    return 0
+  
+  # Get Opinions PAGES
+  def extract_opinions_pages(product_page_soup):
+    opinions_count = int(product_page_soup.find('span', class_="product-review__qo").find('span').text)
+    return opinions_count // 10 + 1 if opinions_count % 10 != 0 else opinions_count // 10
+
+  # Get All Opinions On Pages
+  def extract_opinions(opinionsPageSoup):
+    # - Get HTML for Opinions On Current page
+    htmlOpinions = opinionsPageSoup.find_all('div', class_="js_product-review")
+    # - Parse HTML Opinions
+    parsedOpinions = []
+    for htmlOpinion in htmlOpinions:
+      parsedOpinions.append(Opinion.parse_html_opinion(htmlOpinion))
+    return parsedOpinions
+    
+  # Convert JSON format to CSV or XLSX
+  def convert_json(jsonContent, dataFormat):
+    df = pd.read_json(jsonContent)
+    if dataFormat == "csv":
+      return df.to_csv()
+    elif dataFormat == "xlsx":
+      output = io.BytesIO()
+      df.to_excel(output)
+      return output.getvalue()
+    
+  # Extracts All Product's Opinions
+  def extract_info(self):
+    # - Get HTML Code For Product Page
+    product_page_soup = BeautifulSoup(requests.get(f'https://www.ceneo.pl/{self.id}').text, 'lxml')
+    if product_page_soup.find('div', class_="error-page"):
+      raise InvalidIdError("Invalid id!")
+    self.name = Product.extract_name(product_page_soup)
+    self.averageScore = Product.extract_score(product_page_soup)
+    if product_page_soup.find('li', class_="reviews_new"):
+      return
+    opinionsPages = Product.extract_opinions_pages(product_page_soup)
+    parsedOpinions = []
+    for i in range(1, opinionsPages + 1):
+      opinionsPageSoup = BeautifulSoup(requests.get(f'https://www.ceneo.pl/{self.id}/opinie-{i}').text, 'lxml')
+      parsedOpinions += Product.extract_opinions(opinionsPageSoup)
+    # - Create Opinion Objects And Add Them To Opinions Array
+    for parsedOpinion in parsedOpinions:
+      self.opinions.append(Opinion(*parsedOpinion.values()))
+      
+
+  # Returns Opinions As Dictionaries In A List
+  def get_opinions_dictionary_list(self):
+    opinions_dictionary_list = []
+    for opinion in self.opinions:
+      opinions_dictionary_list.append(opinion.get_opinion_dictionary())
+    return opinions_dictionary_list
+
+  # Return JSON-formatted Opinions
+  def get_opinions_json(self):
+    return json.dumps(self.get_opinions_dictionary_list(), indent=4)
+  
+  # Converts Opinions From JSON format to Opinion Object Format
+  def set_opinions_from_json(self, jsonOpinions):
+    opinions = []
+    for opinion in json.loads(jsonOpinions):
+      opinions.append(Opinion(*opinion.values()))
+    self.opinions = opinions
+    
+  # Returns Products Details (id, name, average score, opinions' count, upsides and downisides count)
+  def get_product_details(self):
+    if self.opinions:
+      
+      df = pd.read_json(self.get_opinions_json())
+      # - Count number of upsides
+      upsidesCount = 0
+      downsidesCount = 0
+      for row in df['upsides']:
+        if row:
+          upsidesCount += len(row.split(','))
+        
+      # - Count number of downsides
+      for row in df['downsides']:
+        if row:
+          downsidesCount += len(row.split(','))
+            
+      return {
+        "id": self.id,
+        "name": self.name,
+        "averageScore": self.averageScore,
+        "opinions_count": len(self.opinions),
+        "upsidesCount": upsidesCount,
+        "downsidesCount": downsidesCount
+      }
+    else:
+      return {
+        "id": self.id,
+        "name": self.name,
+        "averageScore": self.averageScore,
+        "opinions_count": 0,
+        "upsidesCount": 0,
+        "downsidesCount": 0
+      }
+    
+  # Sorts Opinions Depending On Column and Direction
+  def sort_opinions(self, sortColumn, sortDirection):
+    if(self.opinions):
+      opinionsDf = pd.read_json(self.get_opinions_json())
+      sortedOpinions = opinionsDf.sort_values(sortColumn, ascending = False if sortDirection == 'asc' else True ).to_json(orient='records')
+      self.set_opinions_from_json(sortedOpinions)
+    
+  # Filters Opinions Depending On Column and Text
+  def filter_opinions(self, filterColumn, filterText):
+    if(self.opinions):
+      opinionsDf = pd.read_json(self.get_opinions_json())
+      filteredOpinions = opinionsDf.loc[opinionsDf[filterColumn].astype(str).str.contains(filterText)].to_json(orient='records')
+      self.set_opinions_from_json(filteredOpinions)
+    
+  # Counts How Many Of Different Values There Are In Column - AND - Return Dictionary With Different Values As Keys And Their Count As Values
+  def get_counted_column_values_dict(self, column):
+     df = pd.read_json(self.get_opinions_json())
+     return df[column].value_counts().to_dict()
--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,146 @@
+# Import Lib
+from flask import Response, render_template, request, redirect, url_for, Response
 from app import app
-from flask import render_template, request
+import json
+# Import Models
+from app.__init__ import CeneoProduct, db
+from app.models.product import Product
+from app.models.utils import InvalidIdError, ProductAlreadyInDatabase
+from app.models.sortTable import sort_table
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template("index.html")
-
-@app.route('/extraction', methods=["POST", "GET"])
+  return render_template('index.html')
+    
+@app.route("/extraction")
 def extraction():
-    if request.method == "POST":
-        # coś
-        a=0
-    else:
-        return render_template("extraction.html")
+  error = request.args.get("error", None)
+  return render_template('extraction.html', error=error)
 
-@app.route('/product/<product_id>')
-def product():
-    return render_template("product.html")
+@app.route("/extract", methods=["POST", "GET"])
+def extract():
+  if request.method == "POST":
+    try:
+      # - Product Id
+      product_id = int(request.form['product_id'])
+      if not product_id:
+        raise InvalidIdError()
+      if CeneoProduct.query.get(product_id):
+        raise ProductAlreadyInDatabase()
 
-@app.route('/products')
-def products():
-    return render_template("products.html")
+      # - Product in Database  
+      newProduct = Product(product_id)
+      newProduct.extract_info()
+      newCeneoProduct = CeneoProduct(id=product_id, name=newProduct.name, opinions=newProduct.get_opinions_json(), averageScore=newProduct.averageScore)
+      db.session.add(newCeneoProduct)
+      db.session.commit()
+      return redirect(f"/product/{product_id}")
+      # - Some Errors
+    except InvalidIdError:
+      return redirect(url_for('extraction', error="Invalid Product ID"))
+    except ProductAlreadyInDatabase:
+      return redirect(url_for('extraction', error="This Product Has Already Been Extracted And Add To Database"))
+    except OverflowError:
+      return redirect(url_for('extraction', error="Invalid product ID"))
+    except: 
+      return redirect(url_for('error404', error="Problems in Committing to DataBase, Please Check Your Product Id"))
+  else:
+    return redirect("/extraction")
+  
+@app.route("/product/<id>")
+def product(id):
+  try:
+    # - Get All Url Parameters
+    sortColumn = request.args.get('sort_by')
+    sortDirection = request.args.get("direction")
+    filterText = request.args.get('filter')
+    filterColumn = request.args.get("column")
+    # - Search Product from Database And Create a Product Object
+    dbProduct = CeneoProduct.query.get_or_404(id) 
+    productToDisplay = Product(dbProduct.id, dbProduct.name, dbProduct.averageScore)
+    productToDisplay.set_opinions_from_json(dbProduct.opinions)
+    # - Sort And Filter The Opinions According To Url Arguments
+    if sortColumn and sortDirection:
+      productToDisplay.sort_opinions(sortColumn, sortDirection)
+    elif filterText and filterColumn:
+      productToDisplay.filter_opinions(filterColumn, filterText)
+    # - Create a Sortable Table Object And Render Products
+    productTable = sort_table(productToDisplay.opinions, sort_by=sortColumn,sort_reverse=False if sortDirection == 'asc' else True)
+    return render_template('product.html', product=productToDisplay, table=productTable)
+  except:
+    return redirect(url_for('error404', error="There was an issue in loading product data!"))
+  
+@app.route("/products")
+def productsList():
+  try:
+    # - Get Items From Database
+    productsFromDb = CeneoProduct.query.order_by(CeneoProduct.dateCreated).all()
+    # - Get Info For All Products in Database
+    productsInfos = []
+    for productFromDb in productsFromDb:
+      product = Product(productFromDb.id, productFromDb.name, productFromDb.averageScore)
+      product.set_opinions_from_json(productFromDb.opinions)
+      productsInfos.append(product.get_product_details())
+    
+    return render_template("products.html", productsInfos=productsInfos)
+  except:
+    return redirect(url_for('error404', error="There was an issue in loading products!"))
+  
+@app.route("/delete/<int:id>")
+def delete(id):
+  try:
+    productToDelete = CeneoProduct.query.get_or_404(id)
+    db.session.delete(productToDelete)
+    db.session.commit()
+    return redirect("/products")
+  except:
+    return redirect(url_for('error404', error="There was an issue in deleting this product!"))
+  
+@app.route('/download-json/<int:id>')
+def downloadJson(id):
+  try:
+    product = CeneoProduct.query.get_or_404(id)
+    return Response(product.opinions, mimetype="application/json", headers={'Content-Disposition':f'attachment;filename={id}.json'})
+  except:
+    return redirect(url_for('error404', error="Problems with downloading json!"))
+    
+@app.route("/download-csv/<int:id>")
+def downloadCsv(id):
+  try:
+    product = CeneoProduct.query.get_or_404(id)
+    opinionsCsv = Product.convert_json(product.opinions, "csv")
+    return Response(opinionsCsv, headers={'Content-Disposition':f'attachment;filename={id}.csv'})
+  except:
+    return redirect(url_for('error404', error="Problems with downloading csv!"))
+  
+@app.route("/download-xlsx/<int:id>")
+def downloadXlsx(id):
+  try:
+    product = CeneoProduct.query.get_or_404(id)
+    opinionsXlsx = Product.convert_json(product.opinions, 'xlsx')
+    return Response(opinionsXlsx, headers={'Content-Disposition':f'attachment;filename={id}.xlsx'})
+  except:
+    return redirect(url_for('error404', error="Problems with downloading xlsx!"))
+  
 
+@app.route("/charts/<int:id>")
+def plots(id):
+  try:
+    productFromDb = CeneoProduct.query.get_or_404(id)
+    product = Product(productFromDb.id, productFromDb.name, productFromDb.averageScore)
+    product.set_opinions_from_json(productFromDb.opinions)
+    firstChartData = product.get_counted_column_values_dict('recommendation')
+    secondChartData = product.get_counted_column_values_dict('score')
+    
+    return render_template('charts.html', product_id=product.id, firstChartData=json.dumps(firstChartData), secondChartData=json.dumps(secondChartData))
+  except:
+    return redirect(url_for('error404', error="Not found!"))
+    
 @app.route('/about')
 def about():
-    return render_template("about.html")
+  return render_template('about.html')
+
+@app.route('/error404')
+def error404():
+  error = request.args.get("error", None)
+  return render_template('404.html', error=error)
